56c1c9a24851b7bde87102920f797bdc42e35102
Ankita <bobdeankita24@gmail.com>
2024-12-04 00:34:19 -0330

    modify file

---
+++ Test.java
@@ -0,0 +1,8 @@
+//wap to accept marks off tree subjects frome the user.best on the condition print the gread score.
+//gred A if the % above 90
+// b-% above 80
+// c-%above 70
+//d-60
+//for all of other  persentage not allocated
+//programing instutio-use proper class name,use proper and meaning full name frome variable,use proper spacing 
+imop
